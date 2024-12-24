a27366495839aee95feed15055c58bba8c221f0e
theOutcast43 <kokehinc@gmail.com>
2024-12-24 22:55:37 +0100

    fetch specifics from the API

--- index.js
+++ index.js
@@ -6,32 +6,55 @@ function construstURL(key) {
 
 async function getWeatherInfo() {
   const url = construstURL(key)
-  const response = await fetch(url)
-  const jsonData = await response.json()
-  console.log(jsonData)
-  return jsonData
+  try {
+    const response = await fetch(url)
+    const jsonData = await response.json()
+    console.log(jsonData)
+    return jsonData
+  } catch (error) {
+    console.error({'Error Message': error})
+    return null
+  }
 }
 
-function handleSearch() {
+async function storeWeatherData() {
+  const weatherData = await getWeatherInfo()
+  if (weatherData) {
+    const description = weatherData?.description
+    console.log('Description:', description)
+
+    const city = weatherData?.resolvedAddress
+    console.log('City Location:', city)
+
+    const temperature = weatherData?.currentConditions?.temp
+    console.log('Temperature:', temperature)
+    
+    const conditions = weatherData?.currentConditions?.conditions
+    console.log('Conditions:', conditions)
+  }
+}
+
+async function handleSearch() {
   const data = searchBox.value.trim()
   console.log({Search: data})
   if (data) {
     key = data
-    getWeatherInfo()
-    searchBox.value = ''
+    await storeWeatherData()
   } else {
     console.warn('Enter a valid location')
   }
+  searchBox.value = ''
 }
 
 const searchBox = document.getElementById('search-box')
 const btn = document.getElementById('btn')
 
-searchBox.addEventListener('keypress', () => {
+searchBox.addEventListener('keypress', (event) => {
   if (event.key == 'Enter') {
     handleSearch()
   }
 })
 btn.addEventListener('click', handleSearch)
 
-getWeatherInfo()
+// getWeatherInfo()
+storeWeatherData()
